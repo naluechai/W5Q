1b162307726f66b014dbae93a2c0d037ad073efd
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-01-14 15:57:20 +0700

    Storage_number

--- cgi-bin/storagefile.py
+++ cgi-bin/storagefile.py
@@ -2,20 +2,24 @@
 import cgi, cgitb 
 print("Content-type: text/html\r\n\r\n") 
 print("<html><body>")
+
 form = cgi.FieldStorage() 
 number = form.getvalue('input')
+
+r = open('data.txt', 'r')
+s = r.read()
+print("<h1>"+s+"</h1><br>")
+r.close()
 
 print("<form method = 'post' ")
 print("<p>Input: <input type='text' name='input' /></p>") 
 print("<input type='submit' value='Submit' />") 
 print("</form>  ")
-print("Read File Have :")
+print("In File:")
 
 
 f = open('data.txt', 'a+')
 f.write(number)
-s = f.read()
-print(s)
 f.close()
 
 print("</body></html>") 
