e5ebbf313403fab0387303104245e13542f4dd33
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-01-14 16:01:16 +0700

    improve_storage

--- cgi-bin/storagefile.py
+++ cgi-bin/storagefile.py
@@ -6,21 +6,23 @@ print("<html><body>")
 form = cgi.FieldStorage() 
 number = form.getvalue('input')
 
-r = open('data.txt', 'r')
-s = r.read()
-print("<h1>"+s+"</h1><br>")
-r.close()
+
 
 print("<form method = 'post' ")
 print("<p>Input: <input type='text' name='input' /></p>") 
 print("<input type='submit' value='Submit' />") 
 print("</form>  ")
-print("In File:")
-
 
 f = open('data.txt', 'a+')
 f.write(number)
+f.write("<br>")
 f.close()
+
+r = open('data.txt', 'r')
+s = r.read()
+print("In File:")
+print("<h1>"+s+"</h1><br>")
+r.close()
 
 print("</body></html>") 
 
